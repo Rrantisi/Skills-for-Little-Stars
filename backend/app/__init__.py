c7ec022ec57caf6eab29ae3eb331c86df0a03c84
Rrantisi <rantisireham19@gmail.com>
2025-01-13 08:11:40 -0500

    adds flask cors

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,7 +1,10 @@
 from flask import Flask
 from .config import Configuration
 from .routes import main
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
+
 app.config.from_object(Configuration)
 app.register_blueprint(main.bp)
