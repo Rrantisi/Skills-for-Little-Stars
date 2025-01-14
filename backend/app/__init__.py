538872d2b9d3673d1aa6aa288be1f5baad31b6ea
Rrantisi <rantisireham19@gmail.com>
2025-01-14 14:46:59 -0500

    initializes db with the app using the db obj, and sets up flask-migrate to handle migrations

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,10 +1,15 @@
 from flask import Flask
 from .config import Configuration
+from flask_migrate import Migrate
 from .routes import main
 from flask_cors import CORS
+from app.models import db, User
 
 app = Flask(__name__)
 CORS(app)
 
 app.config.from_object(Configuration)
+db.init_app(app)
+Migrate(app, db)
+
 app.register_blueprint(main.bp)
