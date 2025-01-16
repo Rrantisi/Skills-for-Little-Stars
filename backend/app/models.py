0c4b7b140bb00812b2039999afdc7c2c342bfaa2
Rrantisi <rantisireham19@gmail.com>
2025-01-16 07:19:18 -0500

    creates progress class model and updates user model with relationship attribute

--- backend/app/models.py
+++ backend/app/models.py
@@ -16,6 +16,7 @@ class User(db.Model):
     username = db.Column(db.String(100), nullable=False, unique=True)
     hashed_password = db.Column(db.String(255), nullable=False)
     role = db.Column(db.String(20), nullable=False, default='user')
+    progress = db.relationship('Progress', backref='user', lazy=True)
 
     # Get password
     @property
@@ -30,3 +31,12 @@ class User(db.Model):
     # Check password
     def check_password(self, password):
         return bcrypt.check_password_hash(self.hashed_password, password)
+
+class Progress(db.Model):
+    __tablename__ = 'progress'
+
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    score = db.Column(db.Integer)
+    completed = db.Column(db.Boolean, default=False)
+    user = db.relationship('User', backref='progress')
