f8a14f3dea8996f09a56af499ae595e041154cd0
Rrantisi <rantisireham19@gmail.com>
2025-02-07 06:07:17 -0500

    adds carousel, handles next and prev btns, adds styling

--- frontend/src/components/Content/Content.js
+++ frontend/src/components/Content/Content.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchSubjectDetails } from "../../store/subjectReducer";
 import './Content.css';
@@ -9,25 +9,72 @@ function Content({ subjectId, selectedLevel }) {
     (state) => state.subjects.subjectDetails[subjectId]?.[0]?.content[selectedLevel]|| []
   );
 
-  const parsedContent = JSON.parse(content.content);
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  let parsedContent;
+  if(content.content) {
+    parsedContent = JSON.parse(content.content);
+  }
+
+  let totalSlides;
+
+  if(parsedContent?.details.length > 0){
+    totalSlides = parsedContent?.details.length;
+  }
+
+  const setNextSlideIdx = (currentIndex, totalSlides) => {
+    return totalSlides > 0 ? (currentIndex + 1) % totalSlides : 0;
+  };
+
+  const setPrevSlideIdx = (currentIndex, totalSlides) => {
+    return totalSlides > 0 ? (currentIndex - 1 + totalSlides) % totalSlides : 0;
+  };
+
+  const handleNext = () => {
+    setCurrentIndex(setNextSlideIdx(currentIndex, totalSlides))
+  }
+
+  const handlePrev = () => {
+    setCurrentIndex(setPrevSlideIdx(currentIndex, totalSlides))
+  }
+
+  const isValidColor = (color) => {
+    const s = new Option().style;
+    s.color = color;
+    return s.color !== "";
+  }
+
+  const detail = parsedContent?.details[currentIndex] || "";
+
+  const backgroundColor = isValidColor(detail) ? detail : "#2979FF";
 
   useEffect(() => {
     dispatch(fetchSubjectDetails(subjectId));
   }, [dispatch, subjectId]);
 
+  useEffect(() => {
+    setCurrentIndex(0);
+  }, [subjectId, selectedLevel]);
+
+  if (!content || !content.content) return null;
+
   return (
     <>
-      { content && (
+      {content && (
         <div className="content-container">
-          <h2>{ parsedContent?.topic || "" }</h2>
-          <h3>{ parsedContent?.main || "" }</h3>
-          <>
-          { parsedContent?.details.length > 0 && (
-            parsedContent?.details.map((detail) => (
-                <p>{ detail }</p>
-            ))
-          )}
-          </>
+          <h2>{parsedContent?.topic || ""}</h2>
+          <h3>{parsedContent?.main || ""}</h3>
+          <div className="carousel-wrapper">
+            <button className="carousel-btn arrow-left" onClick={handlePrev}>
+              &#10094;
+            </button>
+            <div className="detail-container" style={{ backgroundColor }}>
+              <p>{detail}</p>
+            </div>
+            <button className="carousel-btn arrow-right" onClick={handleNext}>
+              &#10095;
+            </button>
+          </div>
         </div>
       )}
     </>
